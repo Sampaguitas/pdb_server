a1dae8124aab9be8fac8d01e3ffd021c69a02247
Sampaguitas <timothee.desurmont@sampaguitas.com>
2019-01-02 12:35:30 +0400

    password confirmation

--- routes/user/register.js
+++ routes/user/register.js
@@ -12,42 +12,49 @@ const User = require('../../models/User');
 
 
 router.post('/', (req, res) => {
-
-    User.findOne({ email: req.body.email }).then(user => {
-        if (user) {
-            return res.status(400).json({
-                res_no: 100,
-                res_message: fault(100).message
-                //"100": "User already exists"
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.status(400).json({
+            res_no: 105,
+            res_message: fault(105).message
+            //"105": "Password does not match", 
+        });
+    } else {
+        User.findOne({ email: req.body.email }).then(user => {
+            if (user) {
+                return res.status(400).json({
+                    res_no: 100,
+                    res_message: fault(100).message
+                    //"100": "User already exists"
                 });
-        } else {
-            const newUser = new User({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                phone: req.body.phone,
-                email: req.body.email,
-                password: req.body.password,
-                isAdmin:req.body.isAdmin
-            });
-
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err){
-                        return res.status(400).json({
-                            res_no: 106,
-                            res_message: fault(106).message
-                            //"106": "Error generating hashed token"
-                        });
-                    } 
-                    newUser.password = hash;
-                    newUser
-                        .save()
-                        .then(user => res.json(user))      
-                        .catch(err => res.json(err));   
+            } else {
+                const newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    phone: req.body.phone,
+                    email: req.body.email,
+                    password: req.body.password,
+                    isAdmin: req.body.isAdmin
                 });
-            });
-        }
-    });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) {
+                            return res.status(400).json({
+                                res_no: 106,
+                                res_message: fault(106).message
+                                //"106": "Error generating hashed token"
+                            });
+                        }
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => res.json(err));
+                    });
+                });
+            }
+        });
+    }
 });
 
 module.exports = router;

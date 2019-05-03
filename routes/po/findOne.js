abac2abaabfcc605807f680d04ebe8fcf47f9c73
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2019-05-03 12:27:26 +0400

    change

--- routes/po/findOne.js
+++ routes/po/findOne.js
@@ -5,16 +5,17 @@ const fault = require('../../utilities/Errors');
 
 router.get('/', (req, res) => {
     const id = req.query.id
-    Po.findById(id, function (err, po) {
-        if (!po) {
-            return res.status(400).json({ 
-                message: fault(1201).message
-                //"1201": "Po does not exist",
-            });
-        }
-        else {
-            return res.json(po);
-        }
+    Po.findById(id).populate("subs")
+        .exec(function (err, project) {
+            if (!po) {
+                return res.status(400).json({ 
+                    message: fault(1201).message
+                    //"1201": "Po does not exist",
+                });
+            }
+            else {
+                return res.json(po);
+            }
     });
 });
 

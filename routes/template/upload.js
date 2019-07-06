e19116c817704ef1971b19522ace753c984a1e8d
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2019-07-06 18:59:04 +0400

    changes

--- routes/template/upload.js
+++ routes/template/upload.js
@@ -3,8 +3,9 @@ const router = express.Router();
 var aws = require('aws-sdk');
 var path = require('path');
 var multer = require('multer');
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+var multerS3 = require('multer-s3') ////
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 const DocDef = require('../../models/DocDef');
 const fault = require('../../utilities/Errors'); //../utilities/Errors
 const accessKeyId = require('../../config/keys').accessKeyId; //../config/keys
@@ -20,21 +21,21 @@ aws.config.update({
 });
 
 router.post('/', upload.single('file'), function (req, res) {
-  const file = req.body.file;
-  const fileName = req.body.fileName
+  const file = req.file;
   const documentId = req.body.documentId;
   const project = req.body.project;
   
-  DocDef.findOneAndUpdate({_id: documentId}, {field: fileName}, function(err, doc) {
+  DocDef.findOneAndUpdate({_id: documentId}, {field: file.originalname}, function(err, doc) {
     if (err) {
       return res.status(400).json({message: 'unexpected error'});
     } else if (doc) {
       var s3 = new aws.S3();
       var params = {
         Bucket: awsBucketName,
-        Body: file.buffer, //// Body: file.buffer,
-        Key: path.join('templates', project, fileName), //file.originalname
+        Body: file.buffer,
+        Key: path.join('templates', project, file.originalname),
       }; 
+      console.log("originalName:", file.originalname);
       s3.upload(params, function(err, data) {
         if (err) {
           res.status(400).json({message: fault(2405).message}); //"2405": "An error occurred",

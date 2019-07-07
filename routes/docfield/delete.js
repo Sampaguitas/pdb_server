9d76f6a9ee97d048d5f258f6095616118e62a316
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2019-07-07 16:22:18 +0400

    changes

--- routes/docfield/delete.js
+++ routes/docfield/delete.js
@@ -3,22 +3,19 @@ const router = express.Router();
 const DocField = require('../../models/DocField');
 const fault = require('../../utilities/Errors');
 
-router.delete('/', (req, res) => {
-    const id = req.query.id
-    DocField.findByIdAndRemove(id, function (err, docfield) {
-        if (!docfield) {
-            return res.status(400).json({
-                message: fault(2601).message
-                //"2601": "DocField does not exist",
-            });
-        }
-        else {
-            return res.status(200).json({
-                message: fault(2603).message,
-                //"2603": "DocField has been deleted",
-            });
-        }
+router.delete('/', async (req, res) => {
+    const parsedId = JSON.parse(req.query.id);
+    await parsedId.map(id => {
+        DocField.findByIdAndRemove({_id: String(id)}, function(err, fn){
+            if (fn) {
+                console.log('DocField deleted');   
+            }
+            if(err) {
+                return res.status(400).json({message: fault(2601).message}); //"2601": "DocField does not exist",
+            }
+        });
     });
+    return res.status(200).json({message: fault(2603).message}); //"2603": "DocField has been deleted",
 });
 
 module.exports = router;

b31e5fc23b36a97f85f3bf62ea740d83110ba634
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-02-17 11:57:47 +0400

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 
 const app = express();
 
-var whitelist = ['https://www.vanleeuwenpdb.com', 'http://www.vanleeuwenpdb.com', 'http://localhost:8080', 'http://localhost:5555']
+var whitelist = ['https://www.vanleeuwenpdb.com', 'http://www.vanleeuwenpdb.com', 'http://localhost:8080', 'http://localhost:5555', 'https://pdb-client.herokuapp.com/']
 var corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {

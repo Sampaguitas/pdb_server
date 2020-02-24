073348194f51f37dfc2171440fd4f2a0ac48a11a
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-02-24 08:07:53 +0400

    changes

--- routes/supplier/delete.js
+++ routes/supplier/delete.js
@@ -2,23 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Supplier = require('../../models/Supplier');
 const fault = require('../../utilities/Errors');
+const _ = require('lodash');
 
 router.delete('/', (req, res) => {
-    const id = req.query.id
-    Supplier.findByIdAndRemove(id, function (err, supplier) {
-        if (!supplier) {
-            return res.status(400).json({
-                message: fault(1501).message
-                //"1501": "Supplier does not exist",
-            });
-        }
-        else {
-            return res.status(200).json({
-                message: fault(1503).message
-                //"1503": "Supplier has been deleted",
-            });
-        }
-    });
+    const parsedId = JSON.parse(req.query.id);
+    if (_.isEmpty(parsedId)) {
+        return res.status(400).json({message: 'You need to pass an Id'});
+    } else {
+        Supplier.deleteMany({_id: { $in: parsedId } }, function (err) {
+            if (err) {
+                return res.status(400).json({message: 'An error has occured'});
+            } else {
+                return res.status(200).json({message: 'Done'});
+            }
+        });
+    }
 });
 
 module.exports = router;

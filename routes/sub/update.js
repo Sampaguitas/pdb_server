4b0a67fdd2f648ed8911f043e2669cf5f0602b6e
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-02-25 13:18:27 +0400

    set relQty on IRD

--- routes/sub/update.js
+++ routes/sub/update.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const Sub = require('../../models/Sub');
-const fault = require('../../utilities/Errors');
+const _ = require('lodash');
 
 router.put('/', (req, res) => {
     var data = {};
-
+    const id = req.query.id
+    
     Object.keys(req.body).forEach(function (k) {
         data[k] = decodeURI(req.body[k]);
     });
 
-    const id = req.query.id
-    Sub.findByIdAndUpdate(id, { $set: data }, function (err, sub) {
-        if (!sub) {
-            return res.status(400).json({
-                message: fault(1401).message
-                //"1401": "Sub does not exist",
-            });
-        }
-        else {
-            return res.status(200).json({
-                message: fault(1402).message
-                //"1402": "Sub has been updated",
-            });
-        }
-    });
+    if (data.inspRelDate) {
+        Sub.findById(id, function (err, sub) {
+            if (!sub) {
+                return res.status(400).json({ message: 'Could not update Sub information.' });
+            } else {
+                if (sub.rfiQty) {
+                    data.relQty = sub.rfiQty;
+                }
+                Sub.findByIdAndUpdate(id, { $set: data }, function (err, sub) {
+                    if (!sub) {
+                        return res.status(400).json({ message: 'Could not update Sub information.' });
+                    }
+                    else {
+                        return res.status(200).json({ message: 'Sub has been updated.' });
+                    }
+                });
+            }
+        });
+    } else {
+        Sub.findByIdAndUpdate(id, { $set: data }, function (err, sub) {
+            if (!sub) {
+                return res.status(400).json({ message: 'Could not update Sub information.' });
+            }
+            else {
+                return res.status(200).json({ message: 'Sub has been updated.' });
+            }
+        });
+    }
+
+    
+    
 });
 
 module.exports = router;

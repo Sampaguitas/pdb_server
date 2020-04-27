deed1de97bfa11d7341d5dffd43e8f5eaf53d99e
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-04-27 18:39:35 +0400

    changes

---
+++ models/Transaction.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const TransactionSchema = new Schema({  
+    transQty: {
+        type: Number,
+    },
+    transDate: {
+        type: Date,
+    },
+    transType: {
+        type: String,
+        required: true,
+    },
+    nfi: {
+        type: String
+    },
+    plNr: {
+        type: Number,
+    },
+    colliNr: {
+        type: String,
+    },
+    transComment: {
+        type: String,
+        required: true,
+    },
+    locationId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'locations',
+        required: true 
+    },
+    poId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'pos',
+        required: true
+    },
+    subId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'subs',
+    },
+    packitemId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'packitems',
+    },
+    ProjectId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'projects',
+        required: true 
+    }
+});
+
+TransactionSchema.virtual("location", {
+    ref: "locations",
+    localField: "locationId",
+    foreignField: "_id",
+    justOne: true
+});
+
+TransactionSchema.virtual("po", {
+    ref: "pos",
+    localField: "poId",
+    foreignField: "_id",
+    justOne: true
+});
+
+TransactionSchema.virtual("sub", {
+    ref: "subs",
+    localField: "subId",
+    foreignField: "_id",
+    justOne: true
+});
+
+TransactionSchema.virtual("packitem", {
+    ref: "packitems",
+    localField: "packitemId",
+    foreignField: "_id",
+    justOne: true
+});
+
+TransactionSchema.virtual("projectId", {
+    ref: "projects",
+    localField: "ProjectId",
+    foreignField: "_id",
+    justOne: true
+});
+
+module.exports = Transaction = mongoose.model('transactions', TransactionSchema);

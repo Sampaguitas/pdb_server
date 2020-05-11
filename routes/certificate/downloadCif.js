3aa1f4de725b1efb26bb5aa07a53e70865f5fa59
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-05-11 10:34:23 +0400

    changes

---
+++ routes/certificate/downloadCif.js
@@ -0,0 +1,37 @@
+var express = require('express');
+const router = express.Router();
+var aws = require('aws-sdk');
+var path = require('path');
+const accessKeyId = require('../../config/keys').accessKeyId; //../config/keys
+const secretAccessKey = require('../../config/keys').secretAccessKey;
+const region = require('../../config/keys').region;
+const awsBucketName = require('../../config/keys').awsBucketName;
+
+
+aws.config.update({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  region: region
+});
+
+router.get('/', function (req, res) {
+  const id = decodeURI(req.query.id);
+  if (!id) {
+    return res.status(400).json({message: 'Certificate ID is missing.'});
+  } else {
+    
+    var s3 = new aws.S3();
+    var params = {
+        Bucket: awsBucketName,
+        Key: path.join('certificates', `${id}.pdf`),
+    };
+
+    s3.getObject(params).createReadStream()
+    .on('error', () => {
+    res.status(400).json({message: "File could not be located - Please upload a new one"});
+    }).pipe(res);
+
+  }
+});
+
+module.exports = router;

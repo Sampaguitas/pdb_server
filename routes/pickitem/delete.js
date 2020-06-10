737684b4770c89f4a612b1e1c1126eee635294f7
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-06-10 16:07:02 +0400

    changes

---
+++ routes/pickitem/delete.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const PickItem = require('../../models/PickItem');
+const _ = require('lodash');
+
+router.delete('/', async (req, res) => {
+    const selectedIds = req.body.selectedIds
+    let myPromises = [];
+    let nRejected = 0;
+    let nDeleted = 0;
+
+    if (_.isEmpty(selectedIds)) {
+        return res.status(400).json({message: 'You need to pass an Id.'});
+    } else {
+        selectedIds.map(selectedId => myPromises.push(removePickItem(selectedId.pickitemId)));
+        
+        await Promise.all(myPromises).then(function (resPromises) {
+            resPromises.map(function (resPromise) {
+                if (resPromise.isRejected) {
+                    nRejected++;
+                } else {
+                    nDeleted++;
+                }
+            });
+            res.status(!!nRejected ? 400 : 200).json({message: `${nDeleted} item(s) deleted, ${nRejected} item(s) rejected.`});
+        });
+    }
+});
+
+function removePickItem(id) {
+    return new Promise(function(resolve) {
+        PickItem.findByIdAndDelete(id, function (err) {
+            if(err) {
+                resolve({
+                    isRejected: true
+                });
+            } else {
+                resolve({
+                    isRejected: false
+                });
+            }
+        });
+    });
+}
+
+module.exports = router;

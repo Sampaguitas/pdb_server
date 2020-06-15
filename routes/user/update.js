f8658353634d5b3136094d2c34d3273f4a2f3b48
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-06-15 14:05:25 +0400

    changes

--- routes/user/update.js
+++ routes/user/update.js
@@ -12,17 +12,12 @@ router.put('/', (req, res) => {
 
     const id = req.query.id
     User.findOneAndUpdate({_id: id }, { $set: data }, function (err, user) {
-            if (!user) {
-                return res.status(400).json({
-                    message: fault(1601).message
-                    //"1601": "User does not exist",
-                });
-            }
-            else {
-                return res.status(200).json({
-                    message: fault(1602).message
-                    //"1602": "User has been updated",
-                });
+            if (err) {
+                return res.status(400).json({ message: 'An error has occured.'});
+            } if (!user) {
+                return res.status(400).json({ message: 'Could not retreive user to be updated.'});
+            } else {
+                return res.status(200).json({ message: 'User has successfully been updated.' });
             }
     });
 });

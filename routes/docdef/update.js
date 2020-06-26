1ef45ee8fc2cee5bbe83981da5d9b58df9d28c49
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-06-26 12:27:36 +0400

    changes

--- routes/docdef/update.js
+++ routes/docdef/update.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const DocDef = require('../../models/DocDef');
-const fault = require('../../utilities/Errors');
 
 router.put('/', (req, res) => {
     var data = {};
@@ -12,19 +11,12 @@ router.put('/', (req, res) => {
 
     
     const id = req.query.id;
-    console.log('data:', data);
     DocDef.findByIdAndUpdate(id, { $set: data }, function (err, docdef) {
         if (!docdef) {
-            return res.status(400).json({
-                message: 'DocDef does not exist'
-                //"0401": "DocDef does not exist",
-            });
+            return res.status(400).json({ message: 'DocDef does not exist' });
         }
         else {
-            return res.status(200).json({
-                message: 'DocDef has been updated'
-                //"0402": "DocDef has been updated",
-            });
+            return res.status(200).json({ message: 'DocDef has been updated' });
         }
     });
 });

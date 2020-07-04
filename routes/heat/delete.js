d2d1bb93f7d72913e60927ece43895948d9b0853
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-07-04 17:13:50 +0400

    changes

--- routes/heat/delete.js
+++ routes/heat/delete.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const Heat = require('../../models/Heat');
+const _ = require('lodash');
 
-router.delete('/', (req, res) => {
-    const id = req.query.id
-    Heat.findByIdAndDelete(id, function (err, heat) {
-        if (err) {
-            return res.status(400).json({ message: 'An error has occured.' });
-        } else if (!heat) {
-            return res.status(400).json({ message: 'Could not find Heat.' });
-        } else {
-            return res.status(200).json({ message: 'Heat has successfully been deleted.' });
-        }
-    });
+router.delete('/', async (req, res) => {
+    let selectedIds = req.body.selectedIds;
+    let myPromises = [];
+    let nRejected = 0;
+    let nDeleted = 0;
+    if (_.isEmpty(selectedIds)) {
+        res.status(400).json({message: 'You need to pass an Id.'})
+    } else {
+        selectedIds.map(selectedId => myPromises.push(removeHeat(selectedId)));
+        await Promise.all(myPromises).then(results => {
+            results.map(result => {
+                if (result.isRejected) {
+                    nRejected++;
+                } else {
+                    nDeleted++;
+                }
+            });
+            res.status(!!nRejected ? 400 : 200).json({ message: `${nDeleted} heats deleted, ${nRejected} heats deleted.`})
+        });
+    }
 });
 
 module.exports = router;
+
+function removeHeat(selectedId) {
+    return new Promise(function (resolve) {
+        Heat.findByIdAndDelete(selectedId, function (err) {
+            if (err) {
+                resolve({
+                    isRejected: true
+                });
+            } else {
+                resolve({
+                    isRejected: false
+                });
+            }
+        });
+    });
+}

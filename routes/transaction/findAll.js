ae4d138caeb382c41294b18abf9a49d7cd216456
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-07-09 17:32:38 +0400

    changes

--- routes/transaction/findAll.js
+++ routes/transaction/findAll.js
@@ -5,19 +5,22 @@ const Transaction = require('../../models/Transaction');
 router.get('/', (req, res) => {
     Transaction.find({projectId: req.query.projectId})
     .sort({
-        // locationId: 'asc',
-        // transDate: 'asc',
         createdAt: 'asc',
     })
-    .populate({
-        path:'location',
-        populate: {
-            path: 'area',
+    .populate([
+        {
+            path:'location',
             populate: {
-                path: 'warehouse'
+                path: 'area',
+                populate: {
+                    path: 'warehouse'
+                }
             }
+        },
+        {
+            path: 'po',
         }
-    })
+    ])
     .exec(function (err, transaction) {
         if (!!err) {
             return res.status(400).json({ message: "An error has occured." }); 

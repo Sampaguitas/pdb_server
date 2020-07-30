02e596da238552304f0ab2d315b290a38ed73033
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-07-30 12:39:37 +0400

    changes

--- routes/heat/create.js
+++ routes/heat/create.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const Heat = require('../../models/Heat');
 
 router.post('/', (req, res) => {
+    
             const newHeat = new Heat({
                 heatNr: req.body.heatNr,
                 inspQty: req.body.inspQty,
-                certIndex: req.body.certIndex,
-                poId: req.body.poId,
-                subId: req.body.subId,
-                returnId: req.body.returnId,
-                certificateId: req.body.certificateId,
+                certIndex: !!req.body.certIndex ? req.body.certIndex : undefined,
+                poId: !!req.body.poId ? req.body.poId : undefined,
+                subId: !!req.body.subId ? req.body.subId : undefined,
+                returnId: !!req.body.returnId ? req.body.returnId : undefined,
+                certificateId: !!req.body.certificateId ? req.body.certificateId : undefined,
             });
 
             newHeat
                 .save()
                 .then( () => res.status(200).json({message: 'Heat has successfully been created.'}))
-                .catch( () => res.status(400).json({message: 'Heat could not be created.'}));
+                .catch( (err) => {
+                    console.log(err)
+                    res.status(400).json({message: 'Heat could not be created.'})
+                });
 });
 module.exports = router;

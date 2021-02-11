ade51082521b03afc9f864493f2d9359debd8b33
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-02-11 14:22:36 +0400

    changes

--- routes/project/create_temp.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/', async (req, res) => {
-    res.status(400).json({message: "this functionality has temporarily been disabled."});
-});
-
-module.exports = router;
